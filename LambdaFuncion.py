3f167502974bb21588fb7bd9c92cfab860b8143f
K0NGR3SS <157826941+K0NGR3SS@users.noreply.github.com>
2025-06-15 11:32:15 +0100

    Updated bucket upload
    In new version of code, upload function was changed to upload converted file into a new bucket

--- LambdaFuncion.py
+++ LambdaFuncion.py
@@ -5,27 +5,43 @@ import urllib.parse
 s3 = boto3.client('s3')
 polly = boto3.client('polly')
 
-def lambda_handler(event, context): #Context has to be included to match AWS expected handler format
+#Name of bucket for audio upload
+target_bucket = 'Target audio bucket name'
+
+def lambda_handler(event, context):
     print("Event: ", event)
 
-    bucket = event['Records'][0]['s3']['bucket']['name']
+    text_bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote(event['Records'][0]['s3']['object']['key'])
 
     if not key.startswith('notes/'):
         print("Key does not start with 'notes/'")
-        return
+        return {'status': 'skipped', 'reason': 'Key does not start with "notes/"'}
 
-    response = s3.get_object(Bucket=bucket, Key=key)
+    # Reading of .txt files
+    response = s3.get_object(Bucket=text_bucket, Key=key)
     test = response['Body'].read().decode('utf-8')
 
-    # Converting text to speech
+    # Converting text to speech using Amazon Polly
     audio_response = polly.synthesize_speech(
         Text=test,
         OutputFormat='mp3',
         VoiceID = 'Joanna'
     )
 
+    #Forming the key for audio file
     audio_key = key.replace('notes/', 'audio/').replace('.txt', '.mp3')
-    s3.upload_fileobj(audio_response['AudioStream'], Bucket= bucket, Key = audio_key)
 
-    return {'status': 'success', 'audio_key': audio_key}
+    #Uploading MP3 into different bucket
+    s3.upload_fileobj(
+        fileobj = audio_response['AudioStream'],
+        Bucket = target_bucket,
+        Key = audio_key
+    )
+
+    return {
+        'status': 'success',
+        'text_bucket': text_bucket,
+        'audio_bucket': target_bucket,
+        'audio_key': audio_key
+    }
